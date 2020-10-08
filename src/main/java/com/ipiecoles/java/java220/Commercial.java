02bfe7b8d31abd64634e8dedbb08003b799b6ebd
olivierargaud <olivier.argaud@ipilyon.net>
2020-10-08 10:00:45 +0200

    exercice 203 validé

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -1,4 +1,35 @@
 package com.ipiecoles.java.java220;
 
-public class Commercial {
+import org.joda.time.LocalDate;
+
+public class Commercial extends Employe
+{
+
+    private Double caAnnuel;
+
+
+    @Override
+    public Double getPrimeAnnuelle()
+    {
+        if (caAnnuel==null || caAnnuel<10000)
+        {
+            return 500d;
+        }
+        else
+        {
+            return Double.valueOf(Math.ceil(caAnnuel*5/100));
+        }
+    }
+
+    public Double getCaAnnuel()
+    {
+        return caAnnuel;
+    }
+
+    public void setCaAnnuel(Double caAnnuel)
+    {
+        this.caAnnuel = caAnnuel;
+    }
+
+
 }
