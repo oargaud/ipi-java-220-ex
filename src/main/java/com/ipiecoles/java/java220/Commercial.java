d6acdaaba729d3aa06f35a2a42cd86c452bbed56
olivierargaud <olivier.argaud@ipilyon.net>
2020-10-08 10:31:45 +0200

    exercice 206 validé mais 202 et 203 ne le sont plus

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -2,11 +2,47 @@ package com.ipiecoles.java.java220;
 
 import org.joda.time.LocalDate;
 
+import java.util.Objects;
+
 public class Commercial extends Employe
 {
 
     private Double caAnnuel;
+    private Integer performance;
 
+
+    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire,Double caAnnuel)
+    {
+        super( nom, prenom, matricule, dateEmbauche, salaire);
+        this.caAnnuel = caAnnuel;
+
+    }
+
+    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire,Double caAnnuel,Integer performance)
+    {
+        super( nom, prenom, matricule, dateEmbauche, salaire);
+        this.caAnnuel = caAnnuel;
+        this.performance = performance;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        if (!super.equals(o))
+        {
+            return false;
+        }
+        Commercial that = (Commercial) o;
+        return Objects.equals(caAnnuel, that.caAnnuel);
+    }
 
     @Override
     public Double getPrimeAnnuelle()
@@ -19,6 +55,21 @@ public class Commercial extends Employe
         {
             return Double.valueOf(Math.ceil(caAnnuel*5/100));
         }
+    }
+
+    public Boolean performanceEgale(Integer niveauPerformance)
+    {
+        return performance.equals(niveauPerformance);
+    }
+
+
+
+    public Integer getPerformance() {
+        return performance;
+    }
+
+    public void setPerformance(Integer performance) {
+        this.performance = performance;
     }
 
     public Double getCaAnnuel()
